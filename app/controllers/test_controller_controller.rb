c3c9d940dbe0ceb497c03dd1babe4c62a786c42b
Hakimu Jackson <hakimu@newrelic.com>
2015-09-16 10:29:22 -0700

    Updated custom metric to add method tracer to get metric to report to transactions

--- app/controllers/test_controller_controller.rb
+++ app/controllers/test_controller_controller.rb
@@ -1,6 +1,14 @@
+# Added a transaction tracer to this custom metric to get it report as a top level transaction
+require 'new_relic/agent/method_tracer'
+
 class TestControllerController < ApplicationController
+	include NewRelic::Agent::Instrumentation::ControllerInstrumentation
   def display
-  	# @number = rand(100)
-  	::NewRelic::Agent.record_metric('Custom/testing/my_controller',1)
+  	@number = rand(100)
+  	sleep 3
+  	::NewRelic::Agent.record_metric('Custom/testing/my_controller', @number)
   end
+  add_transaction_tracer :display
 end
+
+
